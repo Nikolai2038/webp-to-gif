8e2e13d4dee78b84215a6333aa6e2082e624defb
Nikolai2038 <nikolai2038@yandex.ru>
2024-02-12 15:16:48 +0300

    Fix arguments pass to "test.sh"

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,7 @@ main() {
   directory_with_this_script="$(dirname "${BASH_SOURCE[0]}")"
 
   # Generate new files
-  "${directory_with_this_script}/convert_all_in_dir.sh" "${directory_with_this_script}/../data/examples"
+  "${directory_with_this_script}/convert_all_in_dir.sh" "${directory_with_this_script}/../data/examples" "$@"
 }
 
 main "$@"
